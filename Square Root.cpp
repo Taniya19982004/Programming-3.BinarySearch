f5a483b08605b304875a39c04784de41aa6f9bb0
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2026-01-17 23:08:56 +0530

    Add files via upload

---
+++ Square Root.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        int s=1;
+        int e=n;
+        int ans=1;
+        int mid;
+        
+        while(s<=e){
+             mid = s+(e-s)/2;
+            
+            if(mid*mid<=n){
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter number";
+    cin >> n;
+    Solution obj;
+    int result = obj.floorSqrt(n);
+    cout << "Square root of n : " << result << endl;
+
+    return 0;
+}
